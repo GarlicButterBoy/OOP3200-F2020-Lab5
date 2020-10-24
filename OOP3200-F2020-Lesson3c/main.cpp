f2edf63982876223be5c041eb3316ad91cf5c9cb
Nick Sturch-Flint <nick.rulez@live.com>
2020-10-24 15:11:59 -0400

    Opening and Reading the File is done

--- OOP3200-F2020-Lesson3c/main.cpp
+++ OOP3200-F2020-Lesson3c/main.cpp
@@ -1,5 +1,8 @@
+#include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <map>
+
 
 
 #include "Vector2D.h"
@@ -13,10 +16,12 @@ int main()
 		/************************
 		 *	DECLARATIONS
 		 ************************/
+		//map
+		std::map<std::string, Vector2D<float>*> vectors;
+		//ifstream
+		std::ifstream infile;
+		std::string fileName = "MockDataForTesting.txt";
 
-
-
-		
 		/******************************************************************************
 		 *	Reading Labels/Points into the Map:
 		 *	Attempt to open the data file.  If the data file opens, read a label,
@@ -28,21 +33,51 @@ int main()
 		 *	the correct format. Only continue processing if the file was opened and the
 		 *	map is not empty.
 		 ******************************************************************************/
+		 //attempt to open the file
+		infile.open(fileName.c_str());
 
-		
+		//after file is opened
+		if (infile.is_open())
+		{
+			//declarations
+			std::string name;
+			float x, y;
+			//read the data until there's no data left
+			while (!infile.fail())
+			{
+				//retrieve the data
+				infile >> name;
+				infile.ignore(1, ' ');
+				infile.ignore(1, '(');
+				infile >> x;
+				infile.ignore(1, ',');
+				infile.ignore(1, ' ');
+				infile >> y;
+				infile.ignore(1, ')');
+				//store the data in a map
+				auto* tempVector = new Vector2D<float>(x, y);
+				vectors[name] = tempVector;
+			}
+			infile.close();
+		}
+
+		for (const auto& vector : vectors)
+		{
+			std::cout << vector.first << "   " << vector.second->ToString() << std::endl;
+		}
 
 		/******************************************************************************
 		 *	Determine the Total Distance Between All Points in Order:
 		 *	Use an iterator and a loop to traverse each label/point in the map. For
 		 *	each label/point, determine the distance from that point to the previous
 		 *	point (or next point depending on how you implement this) and add that
-		 *	distance to a total.  Note that the Vector2D class includes a static 
+		 *	distance to a total.  Note that the Vector2D class includes a static
 		 *	distance function to determine the distance between two Vector2D
 		 *	objects, so you should not need to use any complicated math here.  Report
 		 *	to the user how many points the map contains and what the total distance is.
 		 ******************************************************************************/
 
-				
+
 
 		/******************************************************************************
 		 *	Determine the Distance Between the Start Point and a User Selected Point:
@@ -53,20 +88,27 @@ int main()
 		 *	Otherwise, tell the user that the label they entered is not in the map.
 		 *	Repeat these steps until the user enters "quit".
 		 ******************************************************************************/
-		
+
 	}
 	/******************************************************************************
 	 *	Exception Handling:
 	 *	Catch any std::exception thrown. Report to the user that a run-time error
 	 *	occurred and show what exception was thrown.
 	 ******************************************************************************/
-	catch(...)  // an exception was thrown
+	catch (std::exception& ex)  // an exception was thrown
 	{
-		
+		std::cout << ex.what() << std::endl;
 	}
-
+	catch(std::string e)  // an exception was thrown
+	{
+		std::cout << e;
+	}
+	catch (...)  // an exception was thrown
+	{
+		std::cout << "Exception Occured\n";
+	}
 	// END-OF-PROGRAM
-	
+
 	return 0;
 }
 
